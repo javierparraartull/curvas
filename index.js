bad9d5b0571103e870a0bb04499a6d6a65cff65f
javierparraartull <136885036+javierparraartull@users.noreply.github.com>
2023-06-27 21:10:28 +0200

    1 file modified, 1 deleted

--- index.js
+++ index.js
@@ -2,24 +2,59 @@ const canvasSketch = require('canvas-sketch');
 
 const settings = {
 
-  dimmensions:[1080,1080], 
+  dimensions:[1080,1080]
 
 };
 
 const sketch = () => {
 
+
+  const points = [
+
+    new Point({x:200,y:540}),
+    new Point({x:400,y:300, control:true}),
+    new Point({x:880, y:540}),
+  ] 
   return ({context,width,height}) => {
 
-    context.fillStyle = 'black';
+    context.fillStyle = 'white';
     context.fillRect (0,0,width,height);
 
+  //DIBUJA UNA LÍNEA 
+
     context.beginPath();
-    context.moveTo(200,540);
-    context.lineTo(880,540);
+    context.moveTo(points[0].x, points[0].y);
+    context.quadraticCurveTo(points[1].x, points[1].y, points[2].x, points[2].y);
     context.stroke();
-  }
+
+    points.forEach(point => {
+
+      point.draw(context);
+
+    });
+
+  };
+};
 
   canvasSketch(sketch,settings);
 
+// CREAMOS LA CLASE PUNTO PARA TENERLO MÁS ORDENADO. DIBUJAREMOS LOS PUNTOS
+class Point {
+  constructor ({x,y,control=false}){
+    this.x = x;
+    this.y = y;
+    this.control=control;
 
+  }
+
+draw (context)
+{
+  context.save();
+  context.translate(this.x,this.y);
+  content.fillStyle = this.control ? 'red' : 'black';
+
+  context.beginPath();
+  context.arc(0,0,10,0,Math.PI*2);
+  context.fill();
+};
 }
